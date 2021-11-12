d3f50fd5d9c100846742280d4e2d26df961c8c82
kucseranna <kucseranna@gmail.com>
2021-11-12 23:19:55 +0100

    done

---
+++ solutions/ip.js
@@ -0,0 +1,18 @@
+const checkIP = (ip2bChecked) => {
+const ipRegExp = /^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
+return ip2bChecked.match(ipRegExp) ? true : false;
+};
+export default checkIP
+
+
+
+
+/*0.set up everything according to requirements
+1. name parameter
+2. IP: four octets, separated by periods, between 0-255
+3. [0-9] {3} + one more _and_ \.
+4. one octet: 250-555 or 200-249 or 0-199 \.
+4. repeat 3x, plus one more w/o .
+5. return w/
+6. validate IPentry
+7. true or false - ternary*/
